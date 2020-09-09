930e2f69abe63637b71af99280c6437cb60c2759
dfabert <glen.fabert@gmail.com>
2020-09-09 18:39:24 -0400

    all questions entered and general frame of readme is done

---
+++ Title.md
@@ -0,0 +1,22 @@
+
+  # Title
+
+  ### Table of Contents
+
+  ## Description of the Project
+  No available description
+
+  ## Installation Guidelines
+  No instalation guidelines noted
+
+  ## Usage Guidelines
+  No usage guidelines noted
+
+  ## Contributions
+  No contributions noted
+
+  ## Questions
+  https://github.com/dfabert
+  Any further questions, please e-mail me at:  glen.fabert@gmail.com
+  
+  
--- index.js
+++ index.js
@@ -1,36 +1,52 @@
+var fs = require ('fs');
 var inquirer = require("inquirer");
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter title of the project",
-      name: "title"
-    },
-    {
-      type: "input",
-      message: "Enter the description of the project",
-      name: "username"
-    },
-    {
-      type: "input",
-      message: "Enter the installation instructions of the project",
-      name: "installation"
-    },
-    {
-      type: "input",
-      message: "Enter the usage information of the project",
-      name: "usage"
-    },
-    {
-      type: "input",
-      message: "Enter the contributtion guidelines",
-      name: "contribution"
+const questions = [
+  {type: 'input', message:  'Enter the title of the project', name:  'title', default:  'Title'},
+  {type: 'input', message:  'Enter the description of the project', name:  'description', default:  'No available description'},
+  {type: 'input', message:  'Enter the installation instructions of the project', name:  'installation', default:  'No instalation guidelines noted'},
+  {type: 'input', message:  'Enter the usage information fo the project', name:  'usage', default:  'No usage guidelines noted'},
+  {type: 'input', message:  'Enter the contribution guidelines', name:  'contribution', default:  'No contributions noted'},
+  //{type: 'list', message:  'Select License', name:  'License'}
+  {type: 'input', message:  'Enter GitHub username', name:  'gitUser'},
+  {type: 'input', message:  'Enter e-mail address', name:  'email'}
+];
 
-    },
-  ])
-  .then(function(response) {
-    console.log(response.title)
-
-
+inquirer.prompt(questions).then((response) => {
+  const fileName = response.title + '.md';
+  fs.writeFile(fileName, '# ' + response.title, function(err){
+    if (err){
+      return console.log(err);
+    }
   })
+  console.log('A ReadMe file with the name ' + fileName + ' has been created');
+
+  const content = `
+  # ${response.title}
+
+  ## Table of Contents
+
+  ## Description of the Project
+  ${response.description}
+
+  ## Installation Guidelines
+  ${response.installation}
+
+  ## Usage Guidelines
+  ${response.usage}
+
+  ## Contributions
+  ${response.contribution}
+
+  ## Questions
+  https://github.com/${response.gitUser}
+  Any further questions, please e-mail me at:  ${response.email}
+  
+  `;
+
+  fs.writeFile(fileName, content, function(err){
+    if (err) {
+      return console.log(err);
+    }
+  })
+});
