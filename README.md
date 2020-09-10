ef777400c2b2e8278de3e9de38cf1c07b2072e1f
dfabert <glen.fabert@gmail.com>
2020-09-09 21:30:00 -0400

    License and test sections added

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ReadMeGenerator
+
+  
+  # ReadMe Generator
+ 
+  [![The MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+
+  ## Table of Contents
+  * [Description of the Project](#Description-of-the-project)
+  * [Installation Guidelines](#Installation-Guidelines)
+  * [Usage Guidelines](#Usage-Guidelines)
+  * [Contributions](#Contributions)
+  * [License](#License)
+  * [Questions](#Questions)
+
+  ## Description of the Project
+  This application will ask the user 9 questions that will help quickly build a readme file for website applications. After asking the questions using the inquirer.prompt node module, it then creates the file. It will include a badge for the license, a table of contents with links to the other sections, and links to the github repo as well as the creator's e-mail.
+
+  ## Installation Guidelines
+  The user will need node to run this in terminal
+
+  ## Usage Guidelines
+  None noted.
+
+  ## Contributions
+  Derick Fabert contributed to this project
+
+  ## License
+  This application is covered by the following license:  
+  The MIT License  
+  [Click here for more information](https://opensource.org/licenses/MIT)
+
+  ## Questions
+  Github Repository: https://github.com/dfabert  
+  Any further questions, please e-mail me at:  glen.fabert@gmail.com
+  
+  
--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 var fs = require ('fs');
 var inquirer = require("inquirer");
 
+const licenseChoices = [
+  {name:  'MIT', value: {title: 'The MIT License', image:  'https://img.shields.io/badge/License-MIT-yellow.svg', link:'https://opensource.org/licenses/MIT'}},
+  {name:  'Mozilla', value: {title: 'MPL 2.0', image:  'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg', link:'https://opensource.org/licenses/MPL-2.0'}},
+  {name:  'IBM', value: {title: 'IPL 1.0', image:  'https://img.shields.io/badge/License-IPL%201.0-blue.svg', link: 'https://opensource.org/licenses/IPL-1.0'}}
+];
+
 const questions = [
+  {type: 'input', message:  'Enter the file name for the project', name:  'fileName', default:  'README.md'},
   {type: 'input', message:  'Enter the title of the project', name:  'title', default:  'Title'},
   {type: 'input', message:  'Enter the description of the project', name:  'description', default:  'No available description'},
   {type: 'input', message:  'Enter the installation instructions of the project', name:  'installation', default:  'No instalation guidelines noted'},
-  {type: 'input', message:  'Enter the usage information fo the project', name:  'usage', default:  'No usage guidelines noted'},
+  {type: 'input', message:  'Enter the usage information for the project', name:  'usage', default:  'No usage guidelines noted'},
+  {type: 'input', message:  'Enter the test information for the project', name:  'usage', default:  'No test guidelines noted'},
   {type: 'input', message:  'Enter the contribution guidelines', name:  'contribution', default:  'No contributions noted'},
-  //{type: 'list', message:  'Select License', name:  'License'}
+  {type: 'list', message:  'Select License', name:  'License', choices: licenseChoices},
   {type: 'input', message:  'Enter GitHub username', name:  'gitUser'},
   {type: 'input', message:  'Enter e-mail address', name:  'email'}
 ];
 
 inquirer.prompt(questions).then((response) => {
-  const fileName = response.title + '.md';
-  fs.writeFile(fileName, '# ' + response.title, function(err){
-    if (err){
-      return console.log(err);
-    }
-  })
-  console.log('A ReadMe file with the name ' + fileName + ' has been created');
-
+  //Creating the content for the file
   const content = `
+  
   # ${response.title}
+ 
+  [![${response.License.title}](${response.License.image})](${response.License.link})
+  
 
   ## Table of Contents
   * [Description of the Project](#Description-of-the-project)
@@ -45,16 +50,22 @@ inquirer.prompt(questions).then((response) => {
   ${response.contribution}
 
   ## License
+  This application is covered by the following license:  
+  ${response.License.title}  
+  [Click here for more information](${response.License.link})
 
   ## Questions
-  https://github.com/${response.gitUser}
+  Github Repository: https://github.com/${response.gitUser}  
   Any further questions, please e-mail me at:  ${response.email}
   
   `;
 
-  fs.writeFile(fileName, content, function(err){
+  //Placing the content in the file
+  fs.writeFile(response.fileName, content, function(err){
     if (err) {
       return console.log(err);
     }
   })
+
+  console.log('A ReadMe file with the name ' + response.fileName + ' has been created');
 });
